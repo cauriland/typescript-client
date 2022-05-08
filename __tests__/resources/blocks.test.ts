b4b41381a183f3ba9dcc65668c94a9b047d34e66
tnga <devtnga@gmail.com>
2022-05-08 17:01:24 +0100

    add `tests` files and fixtures from code base

---
+++ __tests__/resources/blocks.test.ts
@@ -0,0 +1,36 @@
+import { Blocks } from "../../src/resources/blocks";
+import { configureMocks } from "../mocks";
+
+const resource: Blocks = configureMocks<Blocks>(Blocks);
+
+describe("API - 2.0 - Resources - Blocks", () => {
+	it("should call \"all\" method", async () => {
+		const response = await resource.all();
+
+		expect(response.status).toBe(200);
+	});
+
+	it("should call \"first\" method", async () => {
+		const response = await resource.first();
+
+		expect(response.status).toBe(200);
+	});
+
+	it("should call \"last\" method", async () => {
+		const response = await resource.last();
+
+		expect(response.status).toBe(200);
+	});
+
+	it("should call \"get\" method", async () => {
+		const response = await resource.get("123");
+
+		expect(response.status).toBe(200);
+	});
+
+	it("should call \"transactions\" method", async () => {
+		const response = await resource.transactions("123");
+
+		expect(response.status).toBe(200);
+	});
+});
